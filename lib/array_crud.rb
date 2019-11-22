84e8c3e8d7cdfb394b0ff567b834f329a7ab0bc4
Christopher Morgan <cwm111794@gmail.com>
2019-11-22 07:22:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,8 +15,9 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  
   array.shift(3)
-  puts array[3]
+  
 end
 
 def remove_element_from_start_of_array(array)
